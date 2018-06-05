18e1c55da6ecbcfc307285a7ef8fbed0c54a7f15
realistiqdev <ominousmatt@outlook.com>
2018-06-05 19:31:10 +0100

    Add Bot

---
+++ index.js
@@ -0,0 +1,50 @@
+const Discord = require("discord.js");
+let sent = new Set();
+let cdseconds = 60;
+let channels = new Set();
+
+const client = new Discord.Client({disableEveryone: true});
+
+client.on("ready", async () => {
+  console.log(`${client.user.username} is online!}`);
+});
+
+client.on("message", async message => {
+  if (message.author.bot) return;
+
+  channels.forEach(function(value){
+  if(message.channel.id === value){
+    if(message.member.roles.find("name", "NoRep")) return;
+    if(!sent.has(message.author.id)){
+      message.author.send("Hi there Commander, I am Clyde. Please note that the trade chats have a **1 min cool-down** between every message to reduce text flood.");
+    }
+    sent.add(message.author.id);
+    var cdrole = message.guild.roles.find('name', 'CD');
+
+    message.member.addRole(cdrole);
+
+    setTimeout(() => {
+      message.member.removeRole(cdrole);
+    }, cdseconds * 1000);
+  }
+});
+
+  if(message.member.hasPermissions("ADMINISTRATOR") || message.member.roles.find("name", "Tech-Support")) {
+    const PREFIX = ">";
+    const args = message.content.split(/ +/g);
+    const command = args.shift().toLowerCase();
+
+    if (command === ">cda"){
+      let cdchannel = message.channel.id;
+      channels.add(cdchannel);
+      message.channel.send("Enabled cooldown in this channel.");
+    } else if (command === ">cdd"){
+      let cdchannel = message.channel.id;
+      channels.delete(cdchannel);
+      message.channel.send("Disabled cooldown in this channel.");
+    }
+  }
+
+});
+
+client.login(process.env.BOT_TOKEN);
