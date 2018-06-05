e08e2b9008686e5c80e1ef61512a789a65352f2b
realistiqdev <ominousmatt@outlook.com>
2018-06-05 20:03:28 +0100

    cdl fix

--- index.js
+++ index.js
@@ -43,9 +43,9 @@ client.on("message", async message => {
       channels.delete(cdchannel);
       message.channel.send("Disabled cooldown in this channel.");
     } else if (command === ">cdl"){
-      channels.forEach(function(value)){
+      channels.forEach(function(value){
         message.channel.send(value);
-    }
+    });
   }
 
 });
